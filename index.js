0eba749f579f54af2a40fa540b3636f2b66c5d2f
meilixiaozhang <redpangci@gmail.com>
2020-03-17 15:26:02 +1100

    text manipulation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.getElementsByTagName("li")[2].innerHTML = "xiaozhang";
 document.getElementsByTagName("li")[2].style.color = "pink";
 document.getElementsByClassName("btn")[0].style.color = "red";
-document.getElementById("title").innerHTML = "Good Bye";
+document.getElementById("title").innerHTML = "<em>Good Bye</em>";
 document.querySelector("a").style.color = "green";
 document.querySelector(".btn").innerHTML = "Do Not Click";
 document.querySelector(".list a").innerHTML = "Gooooogle";

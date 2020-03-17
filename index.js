3a567b354a5f5082b58be8259cff0507b4876a62
meilixiaozhang <redpangci@gmail.com>
2020-03-17 15:16:43 +1100

    select element

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 document.getElementsByTagName("li")[2].innerHTML = "xiaozhang";
+document.getElementsByTagName("li")[2].style.color = "pink";
+document.getElementsByClassName("btn")[0].style.color = "red";
+document.getElementById("title").innerHTML = "Good Bye";
+document.querySelector("a").style.color = "green";
+document.querySelector(".btn").innerHTML = "Do Not Click";
+document.querySelector(".list a").innerHTML = "Gooooogle";
+document.querySelector(".btn").style.backgroundColor = "yellow";
